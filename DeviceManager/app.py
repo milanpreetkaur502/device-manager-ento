960fc82b086b504c94791521a12e777e3ba866b3
milanpreet <81068113+milanpreetkaur502@users.noreply.github.com>
2022-05-30 10:51:01 +0530

    Update app.py

--- DeviceManager/app.py
+++ DeviceManager/app.py
@@ -1,9 +1,9 @@
 from crypt import methods
 import json
-from flask import Flask, render_template, Response, redirect, request, session, url_for
+from multiprocessing import dummy
+from flask import Flask, render_template, Response, redirect, request, session, url_for, abort, send_file
 import cv2
 import time
-import subprocess
 import os
 
 app = Flask(__name__)
@@ -63,7 +63,6 @@ def gen_frames():  # generate frame by frame from camera
     
     subprocess.call(["systemctl","stop","cam"])
     camera = cv2.VideoCapture(2)  # use 0 for web camera
-    #camera = cv2.VideoCapture("v4l2src device=/dev/video2 ! video/x-raw, width=640, height=480 framerate=60/1, format=(string)UYVY ! decodebin ! videoconvert ! appsink", cv2.CAP_GSTREAMER)  # use 0 for web camera
     #  for cctv camera use rtsp://username:password@ip_address:554/user=username_password='password'_channel=channel_number_stream=0.sdp' instead of camera
     # for local webcam use cv2.VideoCapture(0)
     while True:
@@ -96,7 +95,18 @@ def video():
 @app.route('/dashboard')
 def dashboard():
     if 'username' in session:
-        return render_template('Dashboard.html',data=readData())
+        dummyData={
+            "cpuInfo":{"usage":5.6},
+            "gpuInfo":{"memoryUsage":2.3},
+            "light_intensity":{"Light_Intensity":6.6},
+            "internet":{"connectivity":True,"signal":2.3},
+            "ramInfo":{"total":4,"usage":3,"free":1},
+            "gps":{"location":{"longitude":1,"latitude":2,"altitude":3}},
+            "temperature":{"Relative_humidity":32,"Temperature_c":21,"Temperature_f":37},
+            "battery_parameters":{"Voltage":2.5,"Internal_temperature":38,"Average_current":2.7},
+            "generalInfo":{"board_serial":34534,"board_type":"NRF","board_revision":2.3}
+        }
+        return render_template('Dashboard.html',data=dummyData)#readData())
     return redirect(url_for('login'))
 
 @app.route('/logout')
@@ -104,6 +114,21 @@ def logout():
     if 'username' in session:
         session.pop('username')
     return redirect(url_for('login'))
+
+@app.route('/files')
+def files():
+    path="/home/attu/Downloads"  #path for the directory's of file
+    if not os.path.exists(path):
+        return abort(404)
+
+    files = os.listdir(path)
+    return render_template('files.html',files=files)
+
+@app.route('/files/<path:filename>', methods=['GET', 'POST'])
+def download(filename):
+    dir="/home/attu/Downloads/"+filename  #path for the directory's of file
+    # Returning file from appended path
+    return send_file(dir)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
